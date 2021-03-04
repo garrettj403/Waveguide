683cc1b3f3a836d17dcbde26a8f8e2c8f7ca45a9
John Garrett <john.garrett@astro.ox.ac.uk>
2021-03-04 16:12:27 -0500

    test: add test for simulated cavity

--- tests/test_cavity.py
+++ tests/test_cavity.py
@@ -1,8 +1,11 @@
 """Tests for cavity sub-module."""
 
+import os
 import numpy as np
 import scipy.constants as sc
 from pytest import approx
+import matplotlib.pyplot as plt
+import skrf as rf
 
 import waveguide as wg
 
@@ -34,7 +37,7 @@ def test_example_6p3():
 
     # Mode numbers
     m, n = 1, 0
-    l = 1
+    ell = 1
 
     # Dielectric properties of polyethylene
     er_mag = 2.25
@@ -49,8 +52,12 @@ def test_example_6p3():
     assert k == approx(157.08, abs=1)
 
     # Resonant frequency
-    fres = wg.resonant_frequency(a, b, d, m=m, n=n, l=l, er=er_mag, ur=1)
+    fres = wg.resonant_frequency(a, b, d, m=m, n=n, l=ell, er=er_mag, ur=1)
     assert fres == approx(f, abs=0.02*sc.giga)
+
+    # Recover permittivity
+    er_mag_recovered = wg.resonant_frequency2permittivity(ell, fres, a, b, d)
+    assert er_mag_recovered == approx(er_mag, abs=0.02)
 
     # Intrinsic impedance
     eta = wg.intrinsic_impedance(er=er_mag, ur=ur)
@@ -69,6 +76,22 @@ def test_example_6p3():
     # Parallel Q-factor
     q_net = wg.qfactor_parallel(qc, qd)
     assert q_net == approx(1927, abs=3)
+
+    # De-embed Q-factor
+    qc_recovered = wg.deembed_qfactor(q_net, qd)
+    assert qc_recovered == approx(qc, abs=10)
+
+    # Recover surface resistance
+    rs_recovered = wg.q2surface_resistance(fres, qc, a, b, d, l=ell, er=er_mag, ur=ur)
+    assert rs_recovered == approx(rs, abs=2e-4)
+
+    # Recover conductivity
+    cond_recovered = wg.q2conductivity(fres, qc, a, b, d, l=ell, er=er_mag, ur=ur)
+    assert cond_recovered == approx(cond, 2e4)
+
+    # Recover loss tangent
+    tand_recovered = wg.q2loss_tangent(qd)
+    assert tand_recovered == approx(tand, abs=2e-4)
 
 
 def test_problem_6p9():
@@ -102,11 +125,150 @@ def test_problem_6p9():
     assert q102 == approx(7987, abs=10)
 
 
-# TODO: add problem 6.23 from Pozar
+def test_problem_6p23():
+    """Test problem 6.23 from Pozar."""
+
+    fres = 9 * sc.giga
+    q = 11_000
+    a, b = 2.5*sc.centi, 1.25*sc.centi
+
+    # Wavenumber
+    k0 = wg.wavenumber(fres, er=1, ur=1)
+    assert k0 == approx(188, abs=5)
+
+    # Phase constant
+    beta0 = wg.phase_constant(fres, a, b, er=1, ur=1, m=1, n=0)
+    assert beta0 == approx(140.5, abs=0.5)
+
+    # Length
+    length = np.pi / beta0
+    assert length == approx(2.24 * sc.centi, abs=0.05 * sc.centi)
+
+
+def test_simulated_cavity(debug=False):
+
+    # Dimensions
+    a, b, d = 280*sc.mil, 140*sc.mil, 6*sc.inch
+    cond = 1e7
+
+    # Load simulated data
+    filename = os.path.join('data', 'cavity.s2p')
+    dir_name = os.path.dirname(__file__)
+    filename = os.path.join(dir_name, filename)
+    data = rf.Network(filename)
+
+    # Unpack
+    f = data.f
+    s21 = data.s[:, 1, 0]
+
+    # Find resonances
+    fres_list = wg.find_resonances(f, wg.db20(s21), height=-90)
+
+    # Get Q-factor
+    fres, q0, ql, _ = wg.find_qfactor(f, np.abs(s21), fres_list, fspan=5e7, ncol=6, figsize=(14,8), debug=debug)
+
+    # Resonant frequencies (theory)
+    ell_start = 3
+    ell = np.arange(ell_start, ell_start + len(fres))
+    fres_theory = wg.resonant_frequency(a, b, d, l=ell)
+    np.testing.assert_almost_equal(fres / 1e9, fres_theory / 1e9, decimal=1)
+
+    # Q-factor (theory)
+    qc_theory = wg.qfactor_conduction(a, b, d, cond, l=ell)
+
+    # Plot Q-factor
+    if debug:
+        plt.figure()
+        plt.plot(ell, qc_theory, 'ko--', label='Theory')
+        plt.plot(ell, q0, 'ro--', label="Q-factor (corrected)")
+        plt.plot(ell, ql, 'bo--', label="Q-factor (loaded)")
+        plt.xlim(xmin=0)
+        plt.legend()
+        plt.show()
+
+    # Get conductivity from Q-factor
+    cond_q = wg.q2conductivity(fres, q0, a, b, d, l=ell)
+    cond_theory = wg.q2conductivity(fres_theory, qc_theory, a, b, d, l=ell)
+
+    # Plot conductivity
+    if debug:
+        plt.figure()
+        plt.plot(fres, cond_q, 'bo-', label="From Q-factor")
+        plt.plot(fres_theory, cond_theory, 'ro--', label="From theory")
+        plt.axhline(cond, c='k', ls='--')
+        plt.ylabel("Conductivity (S/m)")
+        plt.xlabel("Frequency (GHz)")
+        plt.show()
+
+    # Test
+    np.testing.assert_allclose(cond_q, cond*np.ones_like(cond_q), atol=1e6)
+    np.testing.assert_allclose(cond_theory, cond * np.ones_like(cond_q), atol=1)
+
+
+# def test_simulated_cavity_with_hdpe(debug=False):
+#
+#     # Dimensions
+#     a, b, d = 280*sc.mil, 140*sc.mil, 6*sc.inch
+#     cond = 1e7
+#     er
+#
+#     # Load simulated data
+#     filename = os.path.join('data', 'cavity.s2p')
+#     dir_name = os.path.dirname(__file__)
+#     filename = os.path.join(dir_name, filename)
+#     data = rf.Network(filename)
+#
+#     # Unpack
+#     f = data.f
+#     s21 = data.s[:, 1, 0]
+#
+#     # Find resonances
+#     fres_list = wg.find_resonances(f, wg.db20(s21), height=-90)
+#
+#     # Get Q-factor
+#     fres, q0, ql, _ = wg.find_qfactor(f, np.abs(s21), fres_list, fspan=5e7, ncol=6, figsize=(14,8), debug=debug)
+#
+#     # Resonant frequencies (theory)
+#     ell = np.arange(3, 3 + len(fres))
+#     fres_theory = wg.resonant_frequency(a, b, d, l=ell)
+#     np.testing.assert_almost_equal(fres / 1e9, fres_theory / 1e9, decimal=1)
+#
+#     # Q-factor (theory)
+#     qc_theory = wg.qfactor_conduction(a, b, d, cond, l=ell)
+#
+#     # Plot Q-factor
+#     if debug:
+#         plt.figure()
+#         plt.plot(ell, q0, 'bo--', label="Q-factor (corrected)")
+#         plt.plot(ell, ql, 'ro--', label="Q-factor (loaded)")
+#         plt.plot(ell, qc_theory, 'ko--', label='Theory')
+#         plt.xlim(xmin=0)
+#         plt.legend()
+#         plt.show()
+#
+#     # Get conductivity from Q-factor
+#     cond_q = wg.q2conductivity(fres, q0, a, b, d, l=ell)
+#     cond_theory = wg.q2conductivity(fres_theory, qc_theory, a, b, d, l=ell)
+#
+#     # Plot conductivity
+#     if debug:
+#         plt.figure()
+#         plt.plot(fres, cond_q, 'bo-', label="From Q-factor")
+#         plt.plot(fres_theory, cond_theory, 'ro--', label="From theory")
+#         plt.axhline(cond, c='k', ls='--')
+#         plt.ylabel("Conductivity (S/m)")
+#         plt.xlabel("Frequency (GHz)")
+#         plt.show()
+#
+#     # Test
+#     np.testing.assert_allclose(cond_q, cond*np.ones_like(cond_q), atol=1e6)
+#     np.testing.assert_allclose(cond_theory, cond * np.ones_like(cond_q), atol=1)
 
 
 if __name__ == "__main__":
 
-    test_example_6p1()
-    test_example_6p3()
-    test_problem_6p9()
+    # test_example_6p1()
+    # test_example_6p3()
+    # test_problem_6p9()
+    # test_problem_6p23()
+    test_simulated_cavity(debug=True)
