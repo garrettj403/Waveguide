a95f78514f002822af56456a05992167279e6a75
John Garrett <john.garrett@astro.ox.ac.uk>
2021-03-22 15:23:04 -0400

    Add components sub-module

---
+++ waveguide/components.py
@@ -0,0 +1,30 @@
+"""Various waveguide components."""
+
+import scipy.constants as sc
+
+
+def thin_iris(wg, diameter):
+    """Create circular iris Scikit-RF component.
+
+    Args:
+        wg: instance of skrf.RectangularWaveguide class
+        diameter: diameter of circular iris
+
+    Returns:
+        skrf.RectangularWaveguide.line: iris
+
+    """
+
+    radius = diameter / 2
+
+    # Calculate iris inductance
+    alpha_m = 4 * radius ** 3 / 3
+    b_norm_iris = -wg.a * wg.b / (2 * wg.beta * alpha_m)
+    b_iris = b_norm_iris / wg.z0.real
+    l_iris = -1 / b_iris / (2 * sc.pi * wg.frequency.f)
+
+    # Create iris component
+    iris = wg.shunt_inductor(l_iris)
+    iris.name = "Iris: d={:.2f}mm".format(diameter*1e3)
+
+    return iris
